a37be81ec2c2644a53c0de7882af1e94f6f2d133
zh99998 <zh99998@gmail.com>
2016-03-04 11:28:55 +0000

    first commit

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: discourse-email-verify-enhancement
+# about:  https://meta.discourse.org/t/should-we-expect-avatars-to-be-propagated-using-discourse-sso-provider/35332/5?u=zh99998
+# version: 0.1
+# authors: zh99998 <zh99998@gmail.com>
+
+register_asset "javascripts/discourse/templates/modal/not-activated.hbs"
